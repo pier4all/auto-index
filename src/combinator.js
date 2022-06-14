0edbfccd65d4ddd1df6b200a523caeaf703e828a
Lucia de Espona <lucia.espona@gmail.com>
2022-06-14 18:48:26 +0200

    Added tests for the combinator

--- src/combinator.js
+++ src/combinator.js
@@ -48,24 +48,8 @@ class Combinator {
             }
 
             // 2. add sort index (take only latest)
-            let sort_keys = collection_indexes.filter(i => i.operator == '$sort').map(function(i) {return i.key})  
-            let base_sort_keys = []
-            for (let key_obj of sort_keys) {
-                let base_sort_key = {}
-
-                for (let key of Object.keys(key_obj)){
-                    // create a sort index
-                    if (!Object.keys(base_sort_key).includes(key)) {
-                        base_sort_key[key] = key_obj[key]
-                    }
-                    else {
-                        break
-                    }
-                }
-                if (Object.keys(base_sort_key).length > 0) {
-                    base_sort_keys.push(base_sort_key)
-                }
-            }        
+            let base_sort_keys = collection_indexes.filter(i => i.operator == '$sort').map(function(i) {return i.key}) 
+     
             // add only latest
             if (base_sort_keys.length > 0) {
                 let latest_sort_key = base_sort_keys[base_sort_keys.length - 1]
@@ -110,44 +94,22 @@ class Combinator {
 
             let base_lookup_keys = []
             for (let key_obj of lookup_keys) {
-                let base_lookup_key = {}
+                let base_lookup_key = key_obj
 
-                for (let key of Object.keys(key_obj)){
-                    // create a sort index
-                    if (!Object.keys(base_lookup_key).includes(key)) {
-                        base_lookup_key[key] = key_obj[key]
-                    }
-                    else {
-                        break
-                    }
-                }
-                if (Object.keys(base_lookup_key).length > 0) {
-                    base_lookup_keys.push(base_lookup_key)
-                    combinedIndexes.push(new Index('combine_lookup_' + Object.keys(base_lookup_key).join('_') + '_idx', base_lookup_key, collection, "$lookup", 0, {} ))
-                }
+                base_lookup_keys.push(base_lookup_key)
+                combinedIndexes.push(new Index('combine_lookup_' + Object.keys(base_lookup_key).join('_') + '_idx', base_lookup_key, collection, "$lookup", 0, {} ))
             }            
             
-
             // 5. add group index (there is only one)
             let group_keys = collection_indexes.filter(i => i.operator == '$group').map(function(i) {return i.key})  
 
-            let base_group_key = {}
-            for (let key_obj of group_keys) {
-                for (let key of Object.keys(key_obj)){
-                    // create a group index
-                    if (!Object.keys(base_group_key).includes(key)) {
-                        base_group_key[key] = key_obj[key]
-                    }
-                }
-            }    
+            if (group_keys.length> 0) {
+                let base_group_key = group_keys[0]
 
-            if (Object.keys(base_group_key).length > 0) {
                 combinedIndexes.push(new Index('combine_group_' + Object.keys(base_group_key).join('_') + '_idx', base_group_key, collection, "$group", 0, {} ))
-            }
-
-            // 3. Combine group with sort and match indexes
-            let tags = ['$match', '$sort', '$sort$match']
-            if (Object.keys(base_group_key).length > 0){
+                
+                // 3. Combine group with sort and match indexes
+                let tags = ['$match', '$sort', '$sort$match']
                 let i = 0
                 for (let array_keys of [base_match_keys, base_sort_keys, base_sort_match_keys]) {
                     let tag = tags[i]
